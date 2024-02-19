07801b6cae976cc593b7fdc13fd62be95eb40398
Miguel Fialho <miguelfialho16@hotmail.com>
2024-02-19 15:11:11 -0300

    Chapter 2 done

--- books/B1/chpt002.py
+++ books/B1/chpt002.py
@@ -1,4 +1,6 @@
 import turtle
+import math
+
 
 bob = turtle.Turtle()
 
@@ -13,7 +15,6 @@ def square(t, length):
 
 def polygon(t, length, n):
     angle = 360 / n 
-    circumference = length * n 
     for i in range(n):
      t.fd(length)
      t.lt(angle)
@@ -22,7 +23,23 @@ def polygon(t, length, n):
 def circle(t, r):
     polygon(t, r, 60)
 
-circle(bob, 10)
+def circle_remade(t, r):
+    circumference = 2 * math.pi * r
+    n = int(circumference / 3) + 1 
+    length = circumference / n 
+    polygon(t, length, n)
+
+
+
+def arc(t, r, angle):
+    arc_length = 2 * math.pi * r * angle / 360
+    n = int(arc_length / 3) + 1
+    step_length = arc_length / n
+    step_angle  = angle / n 
+    for i in range(n):
+        t.fd(step_length) 
+        t.lt(step_angle)
 
 turtle.mainloop()
+
 
