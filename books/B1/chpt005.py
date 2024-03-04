010c0432f25a1ea32bd6696740fd53999368f8c4
Miguel Fialho <miguelfialho16@hotmail.com>
2024-03-04 15:20:16 -0300

    Chapter 5 exercises almost done

--- books/B1/chpt005.py
+++ books/B1/chpt005.py
@@ -1,4 +1,40 @@
+import math
+
+def print_n(s, n):
+    while n > 0:
+        print(s)
+        n-=1
+
+def mysqrt(a):
+    x = a - 1
+    while True:
+        y = (x + a) / 2
+        if y == x:
+           return y 
+        x = y
+    
+def test_sqrt(q):
+    print("a   " + "mysqrt(a) " + "math.sqrt(a)") 
+    print("- - - - - - - "  + "- "*len("math.sqrt(a)") ) 
+    n = 1 
+    while n < q:
+     x = mysqrt(n) 
+     y = math.sqrt(n) 
+     print(f"{n}   {x}       {y}") 
+     n += 1
+
+def eval_loop():
+    while True:
+        line = str(input("> "))
+        if line.lower() == "done":
+            break
+        print(f">> {eval(line)}")
+
+
+def estimate_pi(k):
+    while k > 1e-15:
+    
 
 
 
-            
+    
