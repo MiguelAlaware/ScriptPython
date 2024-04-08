eaf1058d6d2b21b10c917ec947ec15f013e4515b
Miguel Fialho <miguelfialho16@hotmail.com>
2024-04-08 15:01:16 -0300

    New Chapter 8 exercises done

--- books/B1/chpt008.py
+++ books/B1/chpt008.py
@@ -1,1 +1,18 @@
+def nested_sum(l):
+    c = 0
+    total = 0
+    while len(l) > c:
+        total += sum(l[c])
+        c+=1
+    return total
+
+def cumsum(l):
+    c = 0 
+    while len(l) > c:
+        l[c] = sum(l[0:c+1])
+        c+=1 
+    return l
+
+t = [1, 2, 3]
+print(cumsum(t))
 
