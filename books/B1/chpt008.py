3a73aeb35327e91690ddeb53eb287bad0bb50a94
Miguel Fialho <miguelfialho16@hotmail.com>
2024-04-10 15:10:48 -0300

    Other Chapter 8 exercises done

--- books/B1/chpt008.py
+++ books/B1/chpt008.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def nested_sum(l):
     c = 0
     total = 0
@@ -7,12 +9,52 @@ def nested_sum(l):
     return total
 
 def cumsum(l):
+    new = list() 
     c = 0 
     while len(l) > c:
-        l[c] = sum(l[0:c+1])
-        c+=1 
+         new.append(sum(l[0:c+1])) 
+         c+=1
+    return new 
+
+def middle(l):
+    del l[0]
+    del l[len(l)-1]
     return l
 
-t = [1, 2, 3]
-print(cumsum(t))
+def chop(l):
+    l.remove(l[0])
+    l.remove(l[len(l)-1]) 
+
+def is_sorted(l):
+    ordered_l = sorted(l)
+    if l == ordered_l:
+        return True
+    else:
+        return False
+
+def is_anagram(word_1, word_2):
+    list_1 = list(word_1) 
+    list_2 = list(word_2)
+    if sorted(list_1) == sorted(list_2):
+        return True
+    else:
+        return False
+
+def has_duplicates(l):
+    verification = list()
+    c = 0 
+    while len(l) > c:
+        if l[c] not in verification:
+            verification.append(l[c])
+            c+=1 
+        else:
+            return True
+    return False
+
+
+
+
+
+
+
 
