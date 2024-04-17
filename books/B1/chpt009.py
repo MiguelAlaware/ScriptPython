a0f3271d320bfd65fac034d4ad8b63c2b968d59d
Miguel Fialho <miguelfialho16@hotmail.com>
2024-04-17 15:17:19 -0300

    More Chapter 9 exercises done

--- books/B1/chpt009.py
+++ books/B1/chpt009.py
@@ -4,8 +4,25 @@ def histogram(s):
         d[c] = d.get(c, 0) + 1
     return d
 
-print(histogram('brontosaurs'))
+def read_text(file):
+    words = dict()
+    exec = open(file)   
+    for lines in exec:
+        if lines in words.keys():
+           pass 
+        else:
+           words[lines.strip()] = 1
+    return words
+
+def inverse_dict(d):
+    inverse = dict()
+    for key, val in d.items():
+        inverse.setdefault(val, []).append(key)
+    return inverse
 
 
 
-              
+
+
+
+
