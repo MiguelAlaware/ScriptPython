b56ce41093254afa528aa07ac068a4363b10c4a6
Miguel Fialho <miguelfialho16@hotmail.com>
2024-04-22 15:09:16 -0300

    Chapter 9 done

--- books/B1/chpt009.py
+++ books/B1/chpt009.py
@@ -1,3 +1,5 @@
+
+
 def histogram(s):
     d = dict()
     for c in s: 
@@ -21,8 +23,41 @@ def inverse_dict(d):
     return inverse
 
 
+known = {}
+
+def ackermann(m, n):
+    if (m, n) in known:
+        return known[(m, n)]
+ 
+    elif m == 0:
+        res = n + 1 
+        known[(m, n)] = res
+        return res
+  
+    elif m > 0 and n == 0:
+        res = ackermann(m - 1, 1)
+        known[(m, n)] = res 
+        return res
+   
+    elif m > 0 and n > 0:
+        res = ackermann(m - 1, ackermann(m, n - 1)) 
+        known[(m, n)] = res
+        return res
 
 
+def has_duplicates(l):
+    verification = dict()
+    for i in l:
+        verification[i] = 1
+        if verification[i] in verification:
+            return True
+        else:
+            return False
 
-
+pairs = dict()
+def rotate_word(l): 
+    for i in l:
+        if i == i[::-1]:
+            pairs[i] = i[::-1]
+    return pairs
 
---
+++ books/B1/test.py
@@ -0,0 +1,10 @@
+def ackermann(m, n):
+    if m == 0:
+        return n + 1
+    elif n == 0:
+        return ackermann(m - 1, 1)
+    else:
+        return ackermann(m - 1, ackermann(m, n - 1))
+
+# Testing the function
+print(ackermann(3, 4))  # Output should be 125
